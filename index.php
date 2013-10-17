f4990f702e913d9ac9adcb68f2cb90c55b56749c
jon.huang <sakura.speed@gmail.com>
2013-10-17 17:35:12 +0800

    增加google baidu统计

--- index.php
+++ index.php
@@ -81,11 +81,42 @@ else {
 		$page = preg_replace('/<script[^<]+?pagead2.googlesyndication.com(.+?)script>/', '', $page);
 		$page .= '<style>.ht-news-rolling,.ht-side-bar-sns,.ht-side-bar-code,.ht-buy-list{display:none}</style>';
 		$page .= '<script>$(".ht-suggest-follow").html("<a href=\"http://www.amazon.com/?_encoding=UTF8&camp=1789&creative=9325&linkCode=ur2&tag='.$tag.'\" target=\"_blank\"><img src=/amazon.jpg /></a>")</script>';
+	}
+
+	if ($path == '/'){
+		$page .= <<<EOT
+
+<div id="stat" style="display:none">
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44932079-1', '44zhe.com');
+ga('send', 'pageview');
+
+</script>
+
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fb5021771b4c24818e7503528b034d0a8' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44932079-1', '44zhe.com');
+ga('send', 'pageview');
+
+</script>
+</div>
+EOT;
 
 		header('Content-Length: ' . strlen($page)); //修正页面大小
 	}
-
-	//过滤google
 }
 
 echo $page;
